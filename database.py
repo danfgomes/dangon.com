df1e82f926adb16a3dcc585d571d22ad8b10501a
dani <dg1638382@gmail.com>
2026-07-11 23:27:26 -0300

    Configuring PostgreSQL in the application

--- database.py
+++ database.py
@@ -5,7 +5,6 @@ from config import settings
 
 engine = create_async_engine(
     settings.DATABASE_URL,
-    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = async_sessionmaker(
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from contextlib import asynccontextmanager
 from database import engine
 import models
 from routers import users, posts
+from http import HTTPStatus
+from fastapi.responses import HTMLResponse
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print(" [LIFESPAN] Iniciando a conexão e criando tabelas..")
+    print(" [LIFESPAN] Iniciando a conexo e criando tabelas..")
     async with engine.begin() as conn:
 
         await conn.run_sync(models.Base.metadata.create_all)
@@ -23,6 +25,9 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(users.router)
 app.include_router(posts.router)
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_class=HTMLResponse)
 def init():
-    return {"message": "API rodando com sucesso!"}
+    return """
+    <h1>API rodando com sucesso!</h1>
+
+    """
