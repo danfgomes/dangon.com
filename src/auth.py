564fc313823c3bbe94195291ecb7ec0191303c97
dani <dg1638382@gmail.com>
2026-07-29 00:58:45 -0300

    refactor(auth): add database user lookup and fix session typing in authentication

--- src/auth.py
+++ src/auth.py
@@ -1,15 +1,22 @@
 from datetime import UTC, datetime, timedelta
 
 import jwt
-
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from pwdlib import PasswordHash
+import pwdlib
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from src.models import User
+
 
 from src.config import settings
 
 password_hasher = PasswordHash.recommended()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/users/token")
+DUMMY_HASH = "argon2id$v=19$m=65536,t=3,p=4$Ke04ppvRwTWi1kkItmHb9g$r7yis/cN3wg/UpebnSI3kY98UQuEu+7EMvCTyRzygX4"
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 
 def hash_password(password: str) -> str:
@@ -20,6 +27,20 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_hasher.verify(plain_password, hashed_password)
 
 
+async def authenticate_user(db: AsyncSession, username: str, password: str):
+
+    user = (await db.execute(select(User).where(User.username == username))).scalars().first()
+
+    if not user:
+        verify_password(password, DUMMY_HASH)
+        return False
+
+    if not verify_password(password, user.password):
+        return False
+
+    return user
+
+
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
 
     to_encode = data.copy()
@@ -27,7 +48,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
         expire = datetime.now(UTC) + expires_delta
     else:
         expire = datetime.now(UTC) + timedelta(
-            minutes=settings.access_token_expire_minutes,
+            minutes=settings.access_token_ex,
         )
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
@@ -51,3 +72,19 @@ def verify_access_token(token: str) -> str | None:
         return None
     else:
         return payload.get("sub")
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> int:
+    user_id = verify_access_token(token)
+
+    try:
+        if not user_id:
+            raise ValueError()
+        return int(user_id)
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
